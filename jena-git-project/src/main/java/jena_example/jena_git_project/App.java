6c3bac54aae0e8a99903309bf5170aeaa2f74fc5
arvind088 <avi.lmc098@gmail.com>
2025-04-15 13:32:58 +0200

    Add depedancies in pom file of rdf and implement basic example

---
+++ jena-git-project/src/main/java/jena_example/jena_git_project/App.java
@@ -0,0 +1,13 @@
+package jena_example.jena_git_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
